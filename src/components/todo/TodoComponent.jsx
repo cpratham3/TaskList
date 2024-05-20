e05bfd54e38fb7fe2718681fa21cd5c073db4051
cpratham3 <63926302+cpratham3@users.noreply.github.com>
2024-05-20 13:48:57 -0400

    added addTodo functionality

--- src/components/todo/TodoComponent.jsx
+++ src/components/todo/TodoComponent.jsx
@@ -1,4 +1,4 @@
-import { Formik, Form, Field } from "formik"
+import { Formik, Form, Field, ErrorMessage } from "formik"
 import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 import { retrieveTodoApi } from "./api/TodoApiService"
@@ -28,6 +28,17 @@ export default function TodoComponent() {
         console.log(values);
 
     }
+    function validate(values) {
+        let errors = {
+            description: 'Enter a valid description',
+            targetDate: 'Enter a valid target date'
+        }
+        if (values.description.length < 5) { errors.description = 'Eneter atleast 5 characters' }
+        if (values.targetDate == null ) { errors.targetDate = 'Enter a target date'}
+        console.log(values);
+        return errors
+
+    }
 
     return (
         <div className='container'>
@@ -36,9 +47,23 @@ export default function TodoComponent() {
                 <Formik initialValues={{ description, targetDate }}
                     enableReinitialize={true}
                     onSubmit={onSubmit}
+                    validate={validate}
+                    validateOnChange={false}
+                    validateOnBlur={false}
                 >{
                         (props) => (
                             <Form>
+                                <ErrorMessage
+                                    name='description'
+                                    component='div'
+                                    className='alert alert-warning'
+                                />
+
+                                <ErrorMessage
+                                    name='targetDate'
+                                    component='div'
+                                    className='alert alert-warning'
+                                />
                                 <fieldset className='form-group'>
                                     <label>Description</label>
                                     <Field type='text' className='form-control' name='description' />
