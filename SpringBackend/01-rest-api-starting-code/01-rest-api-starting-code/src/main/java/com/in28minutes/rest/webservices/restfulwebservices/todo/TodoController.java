c5e98f51e8f02bb11234f40a233e3254d8efd7b3
cpratham3 <63926302+cpratham3@users.noreply.github.com>
2024-05-26 20:13:19 -0400

    added database with jpa repo

--- SpringBackend/01-rest-api-starting-code/01-rest-api-starting-code/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/todo/TodoController.java
+++ SpringBackend/01-rest-api-starting-code/01-rest-api-starting-code/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/todo/TodoController.java
@@ -1,54 +1,54 @@
-package com.in28minutes.rest.webservices.restfulwebservices.todo;
-
-import lombok.AllArgsConstructor;
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-
-@RestController
-@RequiredArgsConstructor
-@AllArgsConstructor
-@CrossOrigin(origins = "http://localhost:5173")
-public class TodoController {
-    @Autowired
-    private TodoService todoService;
-    @GetMapping("/users/{username}/todos")
-    public List<Todo> retrieveTodos(@PathVariable String username){
-        return todoService.findByUsername(username);
-
-    }
-
-    @GetMapping("/users/{username}/todos/{id}")
-    public Todo retrieveTodo(@PathVariable String username,@PathVariable int id){
-        return todoService.findById(id);
-
-    }
-
-    @DeleteMapping("/users/{username}/todos/{id}")
-    public ResponseEntity<Void> deleteTodo(@PathVariable String username, @PathVariable int id){
-        todoService.deleteById(id);
-        return ResponseEntity.noContent().build();
-
-    }
-
-    @PutMapping("/users/{username}/todos/{id}")
-    public Todo updateTodo(@PathVariable String username, @PathVariable int id, @RequestBody Todo todo){
-        todoService.updateTodo(todo);
-        return todo;
-
-    }
-
-    @PostMapping("/users/{username}/todos")
-    public Todo createTodo(@PathVariable String username,  @RequestBody Todo todo){
-       Todo createdTodo = todoService.addTodo(username,todo.getDescription(),todo.getTargetDate(),todo.isDone());
-        return createdTodo;
-
-    }
-
-
-
-
-}
+//package com.in28minutes.rest.webservices.restfulwebservices.todo;
+//
+//import lombok.AllArgsConstructor;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.*;
+//
+//import java.util.List;
+//
+//@RestController
+//@RequiredArgsConstructor
+//@AllArgsConstructor
+//@CrossOrigin(origins = "http://localhost:5173")
+//public class TodoController {
+//    @Autowired
+//    private TodoService todoService;
+//    @GetMapping("/users/{username}/todos")
+//    public List<Todo> retrieveTodos(@PathVariable String username){
+//        return todoService.findByUsername(username);
+//
+//    }
+//
+//    @GetMapping("/users/{username}/todos/{id}")
+//    public Todo retrieveTodo(@PathVariable String username,@PathVariable int id){
+//        return todoService.findById(id);
+//
+//    }
+//
+//    @DeleteMapping("/users/{username}/todos/{id}")
+//    public ResponseEntity<Void> deleteTodo(@PathVariable String username, @PathVariable int id){
+//        todoService.deleteById(id);
+//        return ResponseEntity.noContent().build();
+//
+//    }
+//
+//    @PutMapping("/users/{username}/todos/{id}")
+//    public Todo updateTodo(@PathVariable String username, @PathVariable int id, @RequestBody Todo todo){
+//        todoService.updateTodo(todo);
+//        return todo;
+//
+//    }
+//
+//    @PostMapping("/users/{username}/todos")
+//    public Todo createTodo(@PathVariable String username,  @RequestBody Todo todo){
+//       Todo createdTodo = todoService.addTodo(username,todo.getDescription(),todo.getTargetDate(),todo.isDone());
+//        return createdTodo;
+//
+//    }
+//
+//
+//
+//
+//}
--- SpringBackend/01-rest-api-starting-code/01-rest-api-starting-code/target/classes/data.sql
+++ SpringBackend/01-rest-api-starting-code/01-rest-api-starting-code/target/classes/data.sql
@@ -1,2 +1,8 @@
 insert into todo(id,description,done,target_date,username)
 values (10001,'Learn AWS',false,CURRENT_DATE(),'admin');
+
+insert into todo(id,description,done,target_date,username)
+values (10002,'get AWS certified',false,CURRENT_DATE(),'admin');
+
+insert into todo(id,description,done,target_date,username)
+values (10003,'Learn AI',false,CURRENT_DATE(),'admin');
